c56537f867c125c0c98a70103af7f56a67988357
Sindhura Nalluri <snalluri@sitetracker.com>
2021-12-31 18:08:34 +0530

    Added few more templates

--- index.js
+++ index.js
@@ -104,6 +104,24 @@ function generateData(auth) {
                       { name: 'Checklist_Item_Template__cs',
                         range: 'Checklist_Item_Template!A1:X357'
                       },
+                      { name: 'Project_Price_Book__cs',
+                        range: 'Project_Price_Book!A1:F7'
+                      },
+                      { name: 'Pricebook_Item__cs',
+                        range: 'Pricebook_Item!A1:L26'
+                      },
+                      { name: 'Vendor_Payment_Templatecs',
+                        range: 'Vendor_Payment_Template!A1:J30'
+                      },
+                      { name: 'Project_Milestone__cs',
+                        range: 'Project_Milestone!A1:G308'
+                      },
+                      { name: 'Project_Template__cs',
+                        range: 'Project_Template!A1:P17'
+                      },
+                      { name: 'Activity_Template__cs',
+                        range: 'Activity_Template!A1:AU578'
+                      },
                     ];
   const sheets = google.sheets({version: 'v4', auth});
   sheetsList.map((sheetItem) => {
@@ -141,6 +159,24 @@ function generateData(auth) {
               break;
             case sheetsList[8].range: //ChecklistItemTemplateCS
               generate_ChecklistItemTemplateCS_file(sheetItem.name, rows);
+              break;
+            case sheetsList[9].range:
+              generate_ProjectPriceBook_file(sheetItem.name, rows);
+              break;
+            case sheetsList[10].range:
+              generate_PricebookItem_file(sheetItem.name, rows);
+              break;
+            case sheetsList[11].range:
+              generate_VendorPaymentTemplate_file(sheetItem.name, rows);
+              break;
+            case sheetsList[12].range:
+              generate_ProjectMilestoneCS_file(sheetItem.name, rows);
+              break;
+            case sheetsList[13].range:
+              generate_ProjectTemplate_file(sheetItem.name, rows);
+              break;
+            case sheetsList[14].range:
+              generate_ActivityTemplate_file(sheetItem.name, rows);
               break;
             default:
               console.log('No data found.');
@@ -468,6 +504,238 @@ function generate_ChecklistItemTemplateCS_file(filename, data) {
             tempObject['strk__Show_Existing_Related_Records__c'] = item[20];
             tempObject['strk__Read_Only__c'] = item[21];
             tempObject['strk__Request_Signature_for_Children__c'] = item[22];
+            items_cs_data.records.push(tempObject);
+            tempObject = { attributes: {}}
+        }
+        // Generate a new file for every 200 records
+        if(index != 0 && (index % 200 == 0 || index == data.length - 1)) {
+          generateFile(items_cs_data, index, filename);
+          items_cs_data.records = [];
+        }
+      // }
+    });
+  } catch {
+    console.error(newFileName, 'failure');
+  }
+}
+
+function generate_ProjectPriceBook_file(filename, data) {
+  var items_cs_data = { records : []};
+  var tempObject = { attributes: {}};
+  var data, newFileName;
+  try {
+    data.map((item, index) => {
+      if(index != 0) {
+          tempObject.attributes['type'] = 'strk__Project_Price_Book__c';
+          tempObject.attributes['referenceId'] = `Project_Price_Book__cRef${index}`;
+          tempObject['strk__Price_Book_Name__c'] = item[0];
+          tempObject['strk__Active__c'] = item[1];
+          tempObject['strk__Description__c'] = item[2];
+          tempObject['strk__Project_Template__c'] = item[3];
+          tempObject['strk__Pass_Through_Markup_%__c'] = item[4];
+          tempObject['strk__Pass_Through_Markup_Cap__c'] = item[5];
+          items_cs_data.records.push(tempObject);
+          tempObject = { attributes: {}}
+      }
+    });
+      var data = JSON.stringify(items_cs_data);
+      fs.writeFileSync(fileLocation + filename + '.json', data);
+      console.log(filename, ' is generated!');
+  } catch {
+    console.error(newFileName, 'failure');
+  }
+}
+
+function generate_PricebookItem_file(filename, data) {
+  var items_cs_data = { records : []};
+  var tempObject = { attributes: {}};
+  var data, newFileName;
+  try {
+    data.map((item, index) => {
+      if(index != 0) {
+          tempObject.attributes['type'] = 'strk__Pricebook_Item__c';
+          tempObject.attributes['referenceId'] = `Pricebook_Item__cRef${index}`;
+          tempObject['strk__Price_Book_Item__c'] = item[0];
+          tempObject['strk__Project_Template__c'] = item[1];
+          tempObject['strk__Activity_Template__c'] = item[2];
+          tempObject['strk__Project_Price_Book__c'] = item[3];
+          tempObject['strk__Work_Type__c'] = item[4];
+          tempObject['strk__Description__c'] = item[5];
+          tempObject['strk__Amount__c'] = item[6];
+          tempObject['strk__Item_Type__c'] = item[7];
+          tempObject['strk__Auto_Create__c'] = item[8];
+          tempObject['strk__Auto_Create_Criteria__c'] = item[9];
+          tempObject['strk__Auto_Create_Date__c'] = item[10];
+          tempObject['strk__Tags_Applied_On_Upload__c'] = item[11];
+          items_cs_data.records.push(tempObject);
+          tempObject = { attributes: {}}
+      }
+    });
+      var data = JSON.stringify(items_cs_data);
+      fs.writeFileSync(fileLocation + filename + '.json', data);
+      console.log(filename, ' is generated!');
+  } catch {
+    console.error(newFileName, 'failure');
+  }
+}
+
+function generate_VendorPaymentTemplate_file(filename, data) {
+  var items_cs_data = { records : []};
+  var tempObject = { attributes: {}};
+  var data, newFileName;
+  try {
+    data.map((item, index) => {
+      if(index != 0) {
+          tempObject.attributes['type'] = 'strk__Vendor_Payment_Template__c';
+          tempObject.attributes['referenceId'] = `Vendor_Payment_Template__cRef${index}`;
+          tempObject['strk__Expense_Name__c'] = item[0];
+          tempObject['strk__Pricebook_Item__c'] = item[1];
+          tempObject['strk__Description__c'] = item[2];
+          tempObject['strk__Auto_Create__c'] = item[3];
+          tempObject['strk__Auto_Create_Criteria__c'] = item[4];
+          tempObject['strk__Auto_Create_Date__c'] = item[5];
+          tempObject['strk__Default_Cost__c'] = item[6];
+          tempObject['strk__Default_Vendor__c'] = item[7];
+          tempObject['strk__Default_Vendor_Type__c'] = item[8];
+          tempObject['strk__Vendor_Field_API__c'] = item[9];
+          items_cs_data.records.push(tempObject);
+          tempObject = { attributes: {}}
+      }
+    });
+      var data = JSON.stringify(items_cs_data);
+      fs.writeFileSync(fileLocation + filename + '.json', data);
+      console.log(filename, ' is generated!');
+  } catch {
+    console.error(newFileName, 'failure');
+  }
+}
+
+function generate_ProjectMilestoneCS_file(filename, data) {
+  var items_cs_data = { records : []};
+  var tempObject = { attributes: {}};
+  var data, newFileName;
+  try {
+    data.map((item, index) => {
+      // if(index <= 800) {
+        if(index != 0) {
+            tempObject.attributes['type'] = 'strk__Project_Milestone__c';
+            tempObject.attributes['referenceId'] = `Project_Milestone__cRef${index}`;
+            tempObject['strk__Project_Milestone_Name__c'] = item[0];
+            tempObject['strk__sObjectType__c'] = item[1];
+            tempObject['strk__Forecast_Field_API_Name__c'] = item[2];
+            tempObject['strk__Actual_Field_API_Name__c'] = item[3];
+            tempObject['strk__Critical_Milestone__c'] = item[4];
+            tempObject['strk__Project_Milestone_Key__c'] = item[5];
+            tempObject['strk__Milestone_Owner__c'] = item[6];
+            items_cs_data.records.push(tempObject);
+            tempObject = { attributes: {}}
+        }
+        // Generate a new file for every 200 records
+        if(index != 0 && (index % 200 == 0 || index == data.length - 1)) {
+          generateFile(items_cs_data, index, filename);
+          items_cs_data.records = [];
+        }
+      // }
+    });
+  } catch {
+    console.error(newFileName, 'failure');
+  }
+}
+
+function generate_ProjectTemplate_file(filename, data) {
+  var items_cs_data = { records : []};
+  var tempObject = { attributes: {}};
+  var data, newFileName;
+  try {
+    data.map((item, index) => {
+      if(index != 0) {
+          tempObject.attributes['type'] = 'strk__Project_Template__c';
+          tempObject.attributes['referenceId'] = `Project_Template__cRef${index}`;
+          tempObject['strk__Project_Template_Name__c'] = item[0];
+          tempObject['strk__Active__c'] = item[1];
+          tempObject['strk__Sitetracker_Object__c'] = item[2];
+          tempObject['strk__Project_Extension_Objects__c'] = item[3];
+          tempObject['strk__Category__c'] = item[4];
+          tempObject['strk__Complete_Status__c'] = item[5];
+          tempObject['strk__Default_Price_Book__c'] = item[6];
+          tempObject['strk__Manual_Forecast_Change_Behavior__c'] = item[7];
+          tempObject['strk__Schedule_Optional_Column_APIs_CSV__c'] = item[8];
+          tempObject['strk__Schedule_Optional_Column_Labels_CSV__c'] = item[9];
+          tempObject['strk__Schedule_Show_Duration__c'] = item[10];
+          tempObject['strk__Schedule_Show_WBS__c'] = item[11];
+          tempObject['strk__Schedule_Use_Layouts__c'] = item[12];
+          tempObject['strk__Allow_Updates_to_Rendered_Items_Only__c'] = item[13];
+          tempObject['strk__Tags_Applied_On_Upload__c'] = item[14];
+          tempObject['strk__Schedule_Icon_API__c'] = item[15];
+          items_cs_data.records.push(tempObject);
+          tempObject = { attributes: {}}
+      }
+    });
+      var data = JSON.stringify(items_cs_data);
+      fs.writeFileSync(fileLocation + filename + '.json', data);
+      console.log(filename, ' is generated!');
+  } catch {
+    console.error(newFileName, 'failure');
+  }
+}
+
+function generate_ActivityTemplate_file(filename, data) {
+  var items_cs_data = { records : []};
+  var tempObject = { attributes: {}};
+  var data, newFileName;
+  try {
+    data.map((item, index) => {
+      // if(index <= 800) {
+        if(index != 0) {
+            tempObject.attributes['type'] = 'strk__Activity_Template__c';
+            tempObject.attributes['referenceId'] = `Activity_Template__cRef${index}`;
+            tempObject['strk__Activity_Name__c'] = item[0];
+            tempObject['strk__Project_Template__c'] = item[1];
+            tempObject['strk__Project_Milestone__c'] = item[2];
+            tempObject['strk__Field_Name__c'] = item[3];
+            tempObject['strk__Schedule_Width__c'] = item[4];
+            tempObject['strk__Order__c'] = item[5];
+            tempObject['strk__WBS_Code__c'] = item[6];
+            tempObject['strk__Activity_Type__c'] = item[7];
+            tempObject['strk__Sub-Milestone__c'] = item[8];
+            tempObject['strk__Activity_Category__c'] = item[9];
+            tempObject['strk__Activity_Section__c'] = item[10];
+            tempObject['strk__Description__c'] = item[11];
+            tempObject['strk__Activity_Owner_(Field)__c'] = item[12];
+            tempObject['strk__Activity_Owner_(Role)__c'] = item[13];
+            tempObject['strk__Rendered_Logic__c'] = item[14];
+            tempObject['strk__Project_Status__c'] = item[15];
+            tempObject['strk__Enable_Scheduling__c'] = item[16];
+            tempObject['strk__Predecessor_Type__c'] = item[17];
+            tempObject['strk__Predecessor__c'] = item[18];
+            tempObject['strk__Duration__c'] = item[19];
+            tempObject['strk__Days_Type__c'] = item[20];
+            tempObject['strk__Custom_Scheduling_Apex_Class__c'] = item[21];
+            tempObject['strk__Schedule_Using_Predecessor_Forecast_Date__c'] = item[22];
+            tempObject['strk__Require_Completed_Form_to_Actualize__c'] = item[23];
+            tempObject['strk__Actualize_on_Form_Completion__c'] = item[24];
+            tempObject['strk__Form_Template__c'] = item[25];
+            tempObject['strk__Form_Page__c'] = item[26];
+            tempObject['strk__Form_Type__c'] = item[27];
+            tempObject['strk__Job_Template__c'] = item[28];
+            tempObject['strk__Require_Job_Completion_to_Actualize__c'] = item[29];
+            tempObject['strk__Requires_Approval__c'] = item[30];
+            tempObject['strk__Actualize_on_Document_Upload__c'] = item[31];
+            tempObject['strk__Actualize_on_Job_Completion__c'] = item[32];
+            tempObject['strk__Activity_Template_Key__c'] = item[33];
+            tempObject['strk__Auto_Submit_for_Approval__c'] = item[34];
+            tempObject['strk__Cost_Category__c'] = item[35];
+            tempObject['strk__Amount__c'] = item[36];
+            tempObject['strk__Document_Uploads__c'] = item[37];
+            tempObject['strk__Tags_Applied_On_Upload__c'] = item[38];
+            tempObject['strk__Financial_Activity__c'] = item[39];
+            tempObject['strk__Hide_Forecast__c'] = item[40];
+            tempObject['strk__Milestone_Object_Type__c'] = item[41];
+            tempObject['strk__Optional_Activity__c'] = item[42];
+            tempObject['strk__Page_Layout__c'] = item[43];
+            tempObject['strk__Propagate_Name_Changes__c'] = item[44];
+            tempObject['strk__Schedule_Activity_Color__c'] = item[45];
+            tempObject['strk__Email_on_Activity_Start__c'] = item[46];
             items_cs_data.records.push(tempObject);
             tempObject = { attributes: {}}
         }
